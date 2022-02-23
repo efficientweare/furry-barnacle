7e30dd60ed83b8e89051f96b774229b172314a18
efficientweare <56447662+efficientweare@users.noreply.github.com>
2022-02-23 10:25:53 +0800

    bitree.py

---
+++ bitree.py
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct ListNode {
+	int val;
+    struct ListNode *next;
+    ListNode(int x) :
+    val(x), next(NULL) {
+	}
+};
+
+class Solution {
+public:
+    TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
+        TreeNode *tree;
+        
+    }
+};
+
+int main()
+{
+	
+}
